d557669de643e651d03d0bf4f762d9c615ca4625
fahreddinozcan <ozcanfahrettinn@gmail.com>
2024-11-07 16:15:50 +0300

    update example

--- examples/express/index.ts
+++ examples/express/index.ts
@@ -10,12 +10,11 @@ const app = express();
 app.use(express.json());
 
 const someWork = (input: string) => {
-  return `message: '${JSON.stringify(input)}'`
+  return `processed: '${JSON.stringify(input)}'`
 }
 
 app.use('/workflow', serve<{ message: string }>(async (context) => {
   const input = context.requestPayload
-  console.log("input", input);
 
   const result1 = await context.run('step1', async () => {
     const output = someWork(input.message)
@@ -23,11 +22,29 @@ app.use('/workflow', serve<{ message: string }>(async (context) => {
     return output
   })
 
+  const { body } = await context.call("get-data", {
+    url: `${process.env.UPSTASH_WORKFLOW_URL}/get-data`,
+    method: "POST",
+    body: { message: result1 }
+  })
+
   await context.run('step2', async () => {
-    const output = someWork(result1)
+    const { message } = (body as { message: string })
+    const output = someWork(message)
     console.log('step 2 input', result1, 'output', output)
+    return output
   })
 }));
+
+app.post("/get-data", (req, res) => {
+  // Log the incoming request body for debugging
+  console.log('get-data received:', req.body);
+
+  // Send back the message
+  res.json(req.body);
+});
+
+
 
 app.listen(3001, () => {
   console.log('Server running on port 3001');
