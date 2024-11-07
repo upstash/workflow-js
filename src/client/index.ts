353d1f10ed697fe5658e84a74aebedb3bca1dd0a
CahidArda <cahidardaooz@hotmail.com>
2024-11-07 16:43:34 +0300

    feat: add client.trigger

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,10 @@
 import { NotifyResponse, Waiter } from "../types";
 import { Client as QStashClient } from "@upstash/qstash";
 import { makeGetWaitersRequest, makeNotifyRequest } from "./utils";
+import { getWorkflowRunId } from "../utils";
+import { triggerFirstInvocation } from "../workflow-requests";
+import { WorkflowContext } from "../context";
+import { DEFAULT_RETRIES } from "../constants";
 
 type ClientConfig = ConstructorParameters<typeof QStashClient>[0];
 
@@ -87,5 +91,48 @@ export class Client {
    */
   public async getWaiters({ eventId }: { eventId: string }): Promise<Required<Waiter>[]> {
     return await makeGetWaitersRequest(this.client.http, eventId);
+  }
+
+  /**
+   * Trigger new workflow run and returns the workflow run id
+   * 
+   * @param url URL of the workflow
+   * @param body body to start the workflow with
+   * @param headers headers to use in the request
+   * @param workflowRunId optional workflow run id to use. mind that
+   *   you should pass different workflow run ids everytime.
+   * @param retries retry to use in the initial request. in the rest of
+   *   the workflow, `retries` option of the `serve` will be used.
+   * @returns workflow run id
+   */
+  public async trigger({
+    url,
+    body,
+    headers,
+    workflowRunId,
+    retries
+  }: {
+    url: string,
+    body?: unknown,
+    headers?: Record<string, string>,
+    workflowRunId?: string,
+    retries?: number
+  }): Promise<{workflowRunId: string}> {
+    const finalWorkflowRunId = workflowRunId ?? getWorkflowRunId()
+    const context = new WorkflowContext({
+      qstashClient: this.client,
+      // @ts-expect-error headers type mismatch
+      headers: new Headers(headers ?? {}),
+      initialPayload: body,
+      steps: [],
+      url,
+      workflowRunId: finalWorkflowRunId,
+    })
+    const result = await triggerFirstInvocation(context, retries ?? DEFAULT_RETRIES)
+    if (result.isOk()) {
+      return { workflowRunId: finalWorkflowRunId }
+    } else {
+      throw result.error
+    }
   }
 }
