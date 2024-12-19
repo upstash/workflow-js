7396a5d95898534a61cc8dab311cec4ce11bb4f8
Cahid Arda Öz <cahidardaooz@hotmail.com>
2024-12-19 16:19:57 +0300

    Remove tsc dependency and fix path route (#47)
    * fix: nextjs path route
    
    * fix: add test for context.cancel inside context.run

--- examples/nextjs/app/path/route.ts
+++ examples/nextjs/app/path/route.ts
@@ -1,5 +1,3 @@
-export const runtime = 'nodejs';
-
 import { serve } from '@upstash/workflow/nextjs'
 
 const someWork = (input: string) => {
@@ -9,7 +7,13 @@ const someWork = (input: string) => {
 export const { POST } = serve<string>(async (context) => {
   const input = context.requestPayload
   const result1 = await context.run('step1', async () => {
-    console.log(someWork(input));
+    const output = someWork(input)
+    console.log('step 1 input', input, 'output', output)
+    return output
   })
-}, {
+
+  await context.run('step2', async () => {
+    const output = someWork(result1)
+    console.log('step 2 input', result1, 'output', output)
+  })
 })
