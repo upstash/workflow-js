bc569a6a440d5fa7d903a15441ff24d81af48306
fahreddinozcan <ozcanfahrettinn@gmail.com>
2025-01-25 21:28:49 +0300

    chore: seperate router file

--- examples/nextjs/app/test/[[...route]]/route.ts
+++ examples/nextjs/app/test/[[...route]]/route.ts
@@ -1,72 +1,10 @@
-
-import { Env, Hono } from "hono";
-import { serve } from "../../../../../platforms/nextjs"
-import { WorkflowContext } from "../../../../../src/context";
-
+import { W } from "../../../../../src/serve/router"
 
 export type Bindings = {
     DATABASE_URL: string
     REDIS_URL: string
     REDIS_TOKEN: string
 }
-
-type RouterParams = {
-    baseUrl?: string
-    basePath?: string
-}
-
-class W {
-    baseUrl?: string
-    basePath?: string
-    constructor(params?: RouterParams) {
-        const { baseUrl, basePath } = params || {}
-        this.baseUrl = baseUrl
-        this.basePath = basePath
-    }
-
-    router = <T extends Record<string, { payload: unknown; output: unknown }>>(routes: {
-        [K in keyof T]: (context: WorkflowContext<T[K]['payload'], T>) => Promise<T[K]['output']>
-    }) => {
-        const base = new Hono()
-        const route = new Hono()
-
-        Object.entries(routes).forEach(([key, routeFunc]) => {
-            const path = `/${key}` as const
-            route.post(path, async (c) => {
-                const handler = serve(routeFunc, {
-                    baseUrl: this.baseUrl,
-                    router: route as Hono<Env, T>
-                })
-                return await handler.POST(c.req.raw)
-            })
-        })
-
-        if (this.basePath) {
-            base.route(this.basePath, route)
-        }
-
-        const invokeWorkflow = async <K extends keyof T & string>({
-            function: fn,
-            payload
-        }: {
-            function: K
-            payload?: T[K]["payload"]
-        }): Promise<T[K]["output"]> => {
-            const funcURL = this.basePath ? `${this.basePath}/${fn}` : fn
-            const res = await fetch(`${this.baseUrl}/${funcURL}`, {
-                method: 'POST',
-                body: JSON.stringify(payload)
-            })
-            return res.json()
-        }
-
-        return {
-            invokeWorkflow,
-            route: this.basePath ? base : route
-        }
-    }
-}
-
 
 const w = new W({
     baseUrl: "https://5c4c-85-101-27-246.ngrok-free.app",
--- src/serve/router.ts
+++ src/serve/router.ts
@@ -1,5 +1,5 @@
 
-import { Hono } from "hono";
+import { Env, Hono } from "hono";
 import { serve } from "../../platforms/nextjs"
 import { WorkflowContext } from "../context";
 
@@ -10,75 +10,59 @@ export type Bindings = {
 	REDIS_TOKEN: string
 }
 
-class W {
-	constructor() { }
+type RouterParams = {
+	baseUrl?: string
+	basePath?: string
+}
 
-	router = <T extends Record<string, { input: unknown; output: unknown }>>(routes: {
-		[K in keyof T]: (context: WorkflowContext<T[K]['input']>) => Promise<T[K]['output']>
+export class W {
+	baseUrl?: string
+	basePath?: string
+	constructor(params?: RouterParams) {
+		const { baseUrl, basePath } = params || {}
+		this.baseUrl = baseUrl
+		this.basePath = basePath
+	}
+
+	router = <T extends Record<string, { payload: unknown; output: unknown }>>(routes: {
+		[K in keyof T]: (context: WorkflowContext<T[K]['payload'], T>) => Promise<T[K]['output']>
 	}) => {
-		const route = new Hono();
-		Object.entries(routes).forEach(([key, routeFunc]) => {
-			const path = `/${key}` as const;
+		const base = new Hono()
+		const route = new Hono()
 
+		Object.entries(routes).forEach(([key, routeFunc]) => {
+			const path = `/${key}` as const
 			route.post(path, async (c) => {
-				const handler = serve(routeFunc)
+				const handler = serve(routeFunc, {
+					baseUrl: this.baseUrl,
+					router: route as Hono<Env, T>
+				})
 				return await handler.POST(c.req.raw)
 			})
 		})
 
+		if (this.basePath) {
+			base.route(this.basePath, route)
+		}
+
 		const invokeWorkflow = async <K extends keyof T & string>({
 			function: fn,
-			input
+			payload
 		}: {
 			function: K
-			input?: T[K]["input"]
+			payload?: T[K]["payload"]
 		}): Promise<T[K]["output"]> => {
-			const res = await fetch(`/${fn}`, {
+			const funcURL = this.basePath ? `${this.basePath}/${fn}` : fn
+			const res = await fetch(`${this.baseUrl}/${funcURL}`, {
 				method: 'POST',
-				body: JSON.stringify(input)
+				body: JSON.stringify(payload)
 			})
 			return res.json()
 		}
 
-
 		return {
-			invokeWorkflow
+			invokeWorkflow,
+			route: this.basePath ? base : route
 		}
 	}
 }
-
-
-const w = new W()
-const { invokeWorkflow } = w.router<{
-	workflow1: {
-		input: { key: string }
-		output: { value: string }
-	},
-	workflow2: {
-		input: { key: string }
-		output: { value: string }
-	}
-}>({
-	workflow1: async (context) => {
-		return await context.run("WORKFLOW1", async () => {
-			console.log("WORKFLOW 1")
-			return { value: "WORKFLOW 1 RUN" }
-		})
-	},
-	workflow2: async (context) => {
-		return await context.run("WORKFLOW2", async () => {
-			console.log("WORKFLOW 2")
-			return { value: "WORKFLOW 2 RUN" }
-		})
-	}
-})
-
-
-
-
-await invokeWorkflow({
-	function: "workflow1",
-	input: {
-		"key": "value"
-	}
-})
