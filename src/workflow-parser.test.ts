8f72b97874d053506b9bea60d99f56f1e5b347d2
CahidArda <cahidardaooz@hotmail.com>
2025-02-19 15:27:20 +0300

    fix: failure tests

--- src/workflow-parser.test.ts
+++ src/workflow-parser.test.ts
@@ -76,7 +76,7 @@ describe("Workflow Parser", () => {
       expect(throws).toThrow(
         new WorkflowError(
           `Incompatible workflow sdk protocol version.` +
-            ` Expected ${WORKFLOW_PROTOCOL_VERSION}, got ${requestProtocol} from the request.`
+          ` Expected ${WORKFLOW_PROTOCOL_VERSION}, got ${requestProtocol} from the request.`
         )
       );
     });
@@ -726,7 +726,7 @@ describe("Workflow Parser", () => {
       body: btoa(JSON.stringify(formatWorkflowError(new WorkflowError(failMessage)))),
       url: WORKFLOW_ENDPOINT,
       sourceHeader: {
-        Authorization: authorization,
+        [`Upstash-Failure-Callback-Forward-Authorization`]: authorization,
       },
       sourceBody: btoa(
         JSON.stringify([
@@ -787,6 +787,7 @@ describe("Workflow Parser", () => {
     const failureRequest = new Request(WORKFLOW_ENDPOINT, {
       headers: {
         [WORKFLOW_FAILURE_HEADER]: "true",
+        "authorization": authorization
       },
     });
 
@@ -811,8 +812,8 @@ describe("Workflow Parser", () => {
       expect(result.isErr() && result.error.name).toBe(WorkflowError.name);
       expect(result.isErr() && result.error.message).toBe(
         "Workflow endpoint is called to handle a failure," +
-          " but a failureFunction is not provided in serve options." +
-          " Either provide a failureUrl or a failureFunction."
+        " but a failureFunction is not provided in serve options." +
+        " Either provide a failureUrl or a failureFunction."
       );
       expect(called).toBeFalse(); // not called since we threw before auth check
     });
@@ -869,6 +870,8 @@ describe("Workflow Parser", () => {
         {},
         0
       );
+      console.log(result);
+
       expect(result.isOk()).toBeTrue();
       expect(result.isOk() && result.value).toBe("is-failure-callback");
       expect(called).toBeTrue();
@@ -879,7 +882,7 @@ describe("Workflow Parser", () => {
       const routeFunctionWithoutSteps = async (context: WorkflowContext) => {
         called = true;
       };
-      const failureFunction = async () => {};
+      const failureFunction = async () => { };
 
       const result = await handleFailure(
         failureRequest,
@@ -909,7 +912,7 @@ describe("schema validation in serve", () => {
     expectedStatus: number,
     expectedError?: string
   ) => {
-    const { POST } = serve(async () => {}, {
+    const { POST } = serve(async () => { }, {
       ...options,
       env: {
         QSTASH_TOKEN: process.env.QSTASH_TOKEN,
