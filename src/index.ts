61f96df84e16b49da82ff46846cbec6d03e9285b
CahidArda <cahidardaooz@hotmail.com>
2025-10-28 13:01:38 +0300

    feat: add WorkflowRetryAfterError error

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,9 @@ export * from "./types";
 export * from "./client/types";
 export * from "./logger";
 export * from "./client";
-export { WorkflowError, WorkflowAbort, WorkflowNonRetryableError } from "./error";
-export { WorkflowTool } from "./agents/adapters";
+export {
+  WorkflowError,
+  WorkflowAbort,
+  WorkflowNonRetryableError,
+  WorkflowRetryAfterError,
+} from "./error";
